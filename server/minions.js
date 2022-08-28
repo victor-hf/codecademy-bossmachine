1279e2a085bcef1a1293a85032ddef1a29f8aa9d
victor-hf <v.herrero.fernandez@gmail.com>
2022-08-28 21:29:53 +0200

    worked on bonus, could not get put and delete to find correct minion and work

--- server/minions.js
+++ server/minions.js
@@ -3,8 +3,10 @@ const minionsRouter = require('express').Router();
 const {
     addToDatabase,
     getAllFromDatabase,
+    getAllWorkFromDatabase,
     getFromDatabaseById,
     updateInstanceInDatabase,
+    updateWorkInstanceInDatabase,
     deleteFromDatabasebyId,
 } = require('./db');
 
@@ -46,5 +48,49 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
         res.status(404).send();
     }
 });
+
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    let minionWork = getAllWorkFromDatabase(req.minion.id);
+    res.status(200).send(minionWork);
+});
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    let newWork = addToDatabase('work', req.body);
+    res.status(201).send(newWork);
+    console.log(newWork);
+});
+
+minionsRouter.param(':workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    console.log(work);
+    if (work) {
+        req.work = work;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+minionsRouter.put('/:minionId/:workId', (req, res, next) => {
+    if (req.params.minionId !== req.body.minionId) {
+        res.status(400).send();
+    } else {
+        let updatedWork = updateWorkInstanceInDatabase(req.body, req.minion.id);
+        console.log(updatedWork);
+        res.send(updatedWork);
+    }
+});
+
+minionsRouter.delete('/:minionId/:workId', (req, res, next) => {
+    const deletedWork = deleteFromDatabasebyId('work', req.work.id);
+    if (deletedWork) {
+        res.status(204).send(deletedWork);
+    }
+});
+
+// GET /api/minions/:minionId/work to get an array of all work for the specified minon.
+// POST /api/minions/:minionId/work to create a new work object and save it to the database.
+// PUT /api/minions/:minionId/work/:workId to update a single work by id.
+// DELETE /api/minions/:minionId/work/:workId to delete a single work by id
 
 module.exports = minionsRouter;
